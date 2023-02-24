4978bcfe7337171ff45b0b497e38916dd8e59d4c
Sang jin Lee <w1004123@gmail.com>
2023-02-24 22:17:02 +0900

    주석

--- src/App.js
+++ src/App.js
@@ -48,3 +48,81 @@ export default function App () {
     </div>
   )
 }
+
+//클래스형 주석
+
+// import React, {Component} from 'react';
+// import './App.css';
+
+// export default class App extends Component {
+// 	getStyle = (completed) => {
+// 		return {
+// 			padding: '10px',
+// 			borderBottom: '1px #ccc dotted',
+// 			textDecoration: completed ? 'line-through' : 'none',
+// 			paddingBottom: '15px',
+// 		};
+// 	};
+
+// 	state = {
+// 		todoData : [],
+// 		value : ''
+// 	}
+
+// 	handleCompleteChange = (id) => {
+// 		let newTodoData = this.state.todoData.map((data) => {
+// 			if(data.id === id){
+// 				data.completed = !data.completed;
+// 			}
+// 			return data;
+// 		});
+
+// 		this.setState({ todoData : newTodoData });
+// 	}
+
+// 	handleChange = (e) => {
+// 		this.setState({value : e.target.value});
+// 	}
+
+// 	handleClick = (id) => {
+// 		let newTodoData = this.state.todoData.filter(data => data.id !== id);
+// 		console.log(newTodoData);
+// 		this.setState({todoData: newTodoData});
+// 	}
+
+// 	handleOnClick = (e) => {
+// 		e.preventDefault();
+
+// 		let newTodo = {
+// 			id : Date.now(),
+// 			title : this.state.value,
+// 			completed : false,
+// 		}
+
+// 		this.setState({todoData: [...this.state.todoData, newTodo], value: ''});
+// 	}
+// 	render(){
+// 		return(
+// 				<div className='container'>
+// 					<div className='todoBlock'>
+// 						<div className='title'>
+// 							<h1>할 일 목록</h1>
+// 						</div>
+
+// 						<form className='inputForm'>
+// 							<input className='doit' type='text' placeholder='해야 할 일을 입력하세요' value={this.state.value} onChange={this.handleChange}/>
+// 							<input onClick={this.handleOnClick} type='submit' value='입력' className='btn'/>
+// 						</form>
+						
+// 						{this.state.todoData.map((data) => (
+// 							<div style={this.getStyle(data.completed)} key={data.id}>
+// 								<input type='checkbox' defaultChecked={data.completed} onChange={() => this.handleCompleteChange(data.id)}/>
+// 									{data.title}
+// 								<button className='btnStyle' onClick={() => this.handleClick(data.id)}>x</button>
+// 							</div>
+// 						))}
+// 					</div>
+// 				</div>
+// 		  )
+// 	}
+// }
